dcd21a44e20d1b347f334e19bf378cfd53fa3cb7
Juliano Afonso <julianoafonsso1@gmail.com>
2017-09-29 18:14:37 -0300

    Update urls.py

--- Geocodex/Geocodex/Codex/urls.py
+++ Geocodex/Geocodex/Codex/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     url(r'^admin/$', include(admin.site.urls), name='admin'),
     url(r'^index/$', list_target, name='list_target'),
-    url(r'^index/$', target_details, name='target_details')
+    url(r'^details/(?P<target_id>[0-9]+)/$', target_details , name='details'),
 ]
